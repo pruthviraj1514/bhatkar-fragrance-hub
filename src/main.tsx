04de1e9f82d60136fc7184aff94ff5c31a3a8d43
pratiknikam2989 <nikampratik9096@gmail.com>
2026-03-03 09:31:24 +0530

    fix(frontend): Switch to HashRouter to permanently fix 404s on Render Static Sites

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import App from "./App";
 import { AuthProvider } from "@/contexts/AuthContext";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
